3f28eb3ba67bff5fffb85ea87f939499f5c2a8f6
Jonathan Tsai <modopo@gmail.com>
2023-01-24 21:40:47 -0800

    first commit

---
+++ Week_1/class_02/repl.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let cake = {
+  cake: 'birthday cake',
+   slice: 8,
+   icing: 'vanilla',
+   candles: "1000",
+   hasSprinkles: true,
+   allGone: false,
+   raspberryFilling: true,
+  raspberryFilling: null,
+  icecreamOnSide: null,
+}
+
+console.log(typeof (parseInt(cake.candles) + cake.slice));
+
+console.log(typeof null);
